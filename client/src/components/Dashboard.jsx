6aff390ccd8c51d5e2d845cf107cfd2adf609b96
shafee_azeem <shafeeghani@gmail.com>
2025-03-19 01:35:06 +0530

    Dashboard page

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axiosInstance from '../api';
 
 
-const DashBoard = () => {
+const Dashboard = () => {
     const [gallery, setGallery] = useState([]);
 
     //fetch User's gallery
@@ -25,8 +25,23 @@ const DashBoard = () => {
             <h2>Welcome to your DashBoard!</h2>
             <h3>Your Gallery:</h3>
             <div className='gallery'>
+                {gallery.length > 0 ? (
+                    gallery.map((image, index) => (
+                        <div key={index} className="image-card">
+                            <img src={image.url} alt={`image ${index}`} />
+                            <p>{image.title}</p>
+                        </div>
+                    ))
 
+                ) : (
+                    <p>No images found</p>
+                )
+
+                }
 
             </div>
-            );  
+        </div>
+    );
 };
+
+export default Dashboard;
